d028f8e01df9316ffb226734b7c7584d8db4473d
yosefha4 <yosefha4@ac.sce.ac.il>
2024-01-22 13:37:44 +0200

    update depth

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      type: "asset/resource",
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
